c0c897ef7e75f3cf43ace5e520b7603e1ea50830
marti-rgb <megamartish@gmail.com>
2026-05-17 04:45:58 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ import httpx
 from bs4 import BeautifulSoup
 from analyzer import analyze_post
 from database import is_post_processed, mark_post_processed, save_event
-from sheets_config import load_channels, load_keywords, UNAVAILABLE_CHANNELS
+from sheets_config import load_channels, load_keywords, load_categories, match_category_l2, UNAVAILABLE_CHANNELS
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
